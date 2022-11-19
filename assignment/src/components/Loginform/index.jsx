1081bca3fc13c80e80b399783cd56027fdc2d69f
janeesh <janeesh.tin@gmail.com>
2022-11-19 17:44:46 +0530

    final

--- assignment/src/components/Loginform/index.jsx
+++ assignment/src/components/Loginform/index.jsx
@@ -1,22 +1,38 @@
-import {useState} from 'react'
+import { useState } from 'react';
 
-const Loginfrom = () => {
-    useState()
-  return (
-    <div class="login">
-    <form>
-      <h3>Enter Password</h3>
-      <br></br>
-      <input
-        type="password"
-        placeholder="enter your password here.."
-      ></input>
-      <br></br>
-      <br></br>
-      <button>Enter Lab</button>
-    </form>
-  </div>
-  )
-}
+const Loginfrom = ({ animation, setAnimation, setLogin }) => {
+	const [password, setPassword] = useState('');
 
-export default Loginfrom
+	const inputHandler = e => {
+		setPassword(e.target.value);
+	};
+
+	const sumbitHandler = e => {
+		e.preventDefault();
+		if (password === 'skyline') {
+			// animation for door and form
+
+			setTimeout(() => {
+				setLogin(true);
+			}, 6000);
+
+			return setAnimation(true);
+		}
+		return alert('Login Failed, Please enter correct password');
+	};
+
+	return (
+		<div class="login" style={{ animation: animation ? 'fade 2s ease-in-out forwards' : '' }}>
+			<form>
+				<h3>Enter Password</h3>
+				<br></br>
+				<input type="password" placeholder="enter your password here.." onChange={e => inputHandler(e)}></input>
+				<br></br>
+				<br></br>
+				<button onClick={e => sumbitHandler(e)}>Enter Lab</button>
+			</form>
+		</div>
+	);
+};
+
+export default Loginfrom;
--- assignment/src/components/homescreen/Homescreen.jsx
+++ assignment/src/components/homescreen/Homescreen.jsx
@@ -1,19 +1,39 @@
-import React from "react";
-import Loginfrom from "../Loginform";
-import "./homescreen.css";
+import React from 'react';
+import Loginfrom from '../Loginform';
+import './homescreen.css';
 
 const Homescreen = () => {
-  return (
-    <div class="main">
-      <div class="parts">
-        <button>EXIT</button>
-        <h1 id="countdown">00:10:00</h1>
-      </div>
-      <div class="left"></div>
-      <div class="right"></div>
-      <Loginfrom />
-    </div>
-  );
+	const [animation, setAnimation] = React.useState(false);
+
+	const getlogin = () => {
+		return localStorage.getItem('login');
+	};
+
+	const setLogin = login => {
+		localStorage.setItem('login', login);
+	};
+
+	return (
+		<div className="main">
+			<div className="parts">
+				<button>EXIT</button>
+				<h1 id="countdown">00:10:00</h1>
+			</div>
+
+			{getlogin() === 'true' ? null : (
+				<>
+					<div
+						className="left"
+						style={{ animation: animation ? 'leftroom 5s ease-in-out forwards' : '' }}></div>
+					<div
+						className="right"
+						style={{ animation: animation ? 'rightroom 5s ease-in-out forwards' : '' }}></div>
+
+					<Loginfrom animation={animation} setAnimation={setAnimation} setLogin={setLogin} />
+				</>
+			)}
+		</div>
+	);
 };
 
 export default Homescreen;
