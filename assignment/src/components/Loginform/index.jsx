979c3ac0ec6082b49e61d8778e4e3c12169658ee
janeesh <janeesh.tin@gmail.com>
2022-11-19 16:13:34 +0530

    login

---
+++ assignment/src/components/Loginform/index.jsx
@@ -0,0 +1,22 @@
+import {useState} from 'react'
+
+const Loginfrom = () => {
+    useState()
+  return (
+    <div class="login">
+    <form>
+      <h3>Enter Password</h3>
+      <br></br>
+      <input
+        type="password"
+        placeholder="enter your password here.."
+      ></input>
+      <br></br>
+      <br></br>
+      <button>Enter Lab</button>
+    </form>
+  </div>
+  )
+}
+
+export default Loginfrom
--- assignment/src/components/homescreen/Homescreen.jsx
+++ assignment/src/components/homescreen/Homescreen.jsx
@@ -1,30 +1,17 @@
 import React from "react";
+import Loginfrom from "../Loginform";
 import "./homescreen.css";
 
 const Homescreen = () => {
   return (
     <div class="main">
-     
       <div class="parts">
-      <button>EXIT</button>
-      <h1 id="countdown">00:10:00</h1>
+        <button>EXIT</button>
+        <h1 id="countdown">00:10:00</h1>
       </div>
       <div class="left"></div>
       <div class="right"></div>
-
-      <div class="login">
-        <form>
-          <h3>Enter Password</h3>
-          <br></br>
-          <input
-            type="password"
-            placeholder="enter your password here.."
-          ></input>
-          <br></br>
-          <br></br>
-          <button>Enter Lab</button>
-        </form>
-      </div>
+      <Loginfrom />
     </div>
   );
 };
